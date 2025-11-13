0c43dfad5c89e7996f22d405f1829f38a764863a
Soujanya <rnaiksoujanya@gmail.com>
2025-11-13 10:25:35 +0530

    Update Authentication.ino

--- Authentication.ino
+++ Authentication.ino
@@ -249,13 +249,16 @@ void class5() {
 
 int getFingerprintIDez() {
   uint8_t p = finger.getImage();
-  if (p != FINGERPRINT_OK) return -1;
+  if (p != FINGERPRINT_OK) 
+    return -1;
 
   p = finger.image2Tz();
-  if (p != FINGERPRINT_OK) return -1;
+  if (p != FINGERPRINT_OK)
+    return -1;
 
   p = finger.fingerFastSearch();
-  if (p != FINGERPRINT_OK) return -1;
+  if (p != FINGERPRINT_OK)
+    return -1;
 
   return finger.fingerID;
 }
